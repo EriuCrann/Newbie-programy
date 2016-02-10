5244d637c5c671c0c68e286b9d0be732f1a36bef
Tom Fanta <tomfantadev@gmail.com>
2016-02-10 19:47:49 +0100

    Niflheim project

--- Tox/Niflheim/Niflheim/Program.cs
+++ Tox/Niflheim/Niflheim/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
 namespace Niflheim
-{
+{	
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+		{			
+			Console.WriteLine ("Welcome to Niflheim!");
+
+			int b = 0;
+			int a = 0;
+
+			a = 5;
+
+			int c = a + b;
+
+			Console.WriteLine ("a: {0} ",a);
+			Console.WriteLine ("a: " + a);
+			Console.WriteLine ("a: {0}, b: {1}, c: {2}. ({0} + {1} = {2})", a, b, c);
+
+			bool isOpen = true;
+			string input = Console.ReadLine ();
+
+			if( input == "open")
+			{			
+				isOpen = true;
+			}
+			else if (input == "close")
+			{
+				isOpen = false;
+			}
+
+			if (isOpen == true) 
+			{
+				Console.WriteLine ("Je otevřeno, zavírám...");
+				isOpen = false;
+			} 
+			else 
+			{
+				Console.WriteLine ("Je zavřeno, otevírám...");
+				isOpen = true;
+			}
+
+			Console.WriteLine (isOpen);
+
+
+			Console.ReadKey ();
 		}
 	}
 }
